3206b90a4c2129308d451b5a2c2e09b6343cd1f1
RibieSantos <johnsantos.JS288@gmail.com>
2023-04-17 11:55:09 +0800

    updated again

---
+++ POS_ETR/Connection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_ETR
+{
+	internal class Connection
+	{
+		private SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\johns\source\repos\POS_ETR\POS_ETR\sampleDB.mdf;Integrated Security=True;Connect Timeout=30");
+
+		
+
+	}
+}
